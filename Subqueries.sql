b0bb3d24095c493c5b1c21593acc0640a16706d4
Olga Lazarenko <olgal1999@yahoo.com>
2018-01-26 14:12:23 -0500

    Subqueries at SELECT, FROM, WHERE line

--- Subqueries.sql
+++ Subqueries.sql
@@ -21,9 +21,39 @@ WHERE BusinessEntityID IN
 	(
 	SELECT BusinessEntityID AS EmployeeID --this subquery/inner query has no dependency on the outer query
 	FROM AdventureWorks2012.HumanResources.EmployeePayHistory 
-	WHERE Rate > 20  -- this subquery is multi-valued, it returnes multiple values
+	WHERE Rate > 20  -- this subquery is multi-valued, it returns multiple values
 	) ; 
-	-- returned 79 rows
+-- returned 79 rows
 
+
+-- ****************************************************************************************************************
+-- Subquery can be placed in the lines of the following causes: SELECT, FROM, WHERE
+--#2. Subquery at  SELECT line
+
+ SELECT ProductID	
+	, Name AS 'ProductName'
+	, ProductNumber
+	, ListPrice
+	, ( StandardCost -
+	  (SELECT AVG(StandardCost)
+	  FROM AdventureWorks2012.Production.Product ) ) -- the inner query is the subquery for the outer quiery
+		AS StandardCostDifference                    -- it is a self-contained query that returns a scalar value
+FROM AdventureWorks2012.Production.Product ;
+
+
+--#3. Subquery at SELECT clause line
+
+SELECT TerritoryID
+	, Name AS 'TerritoryName'
+	, CountryRegionCode AS 'CountryCode'
+	, (SalesYTD - 
+				( SELECT AVG(SalesYTD) -- this is the inner self-contained subquery, returns the single value
+					FROM AdventureWorks2012.Sales.SalesTerritory)) AS 'SalesDiffernce'
+FROM AdventureWorks2012.Sales.SalesTerritory ; 
+
+
+
+--************************************************************************************************************
+-
 
  

6b3b87f5d7e23ec1a692106872523bd7737844ef
Olga Lazarenko <olgal1999@yahoo.com>
2018-02-07 13:27:08 -0500

    ROLLUP clause

--- Multiple Grouping Sets/Multiple Grouping Sets.sql
+++ Multiple Grouping Sets/Multiple Grouping Sets.sql
@@ -38,3 +38,99 @@ ORDER BY DepartmentID , YEAR(StartDate) ;
 
 SELECT Distinct(DepartmentID) 
 FROM AdventureWorks2012.HumanResources.EmployeeDepartmentHistory ; 
+
+
+
+-- ********************************************************************************************
+--ROLLUP clause is used to have aggregations for each grouping element and the final aggregation
+    SELECT DepartmentName AS Department
+	, Gender
+	, MaritalStatus
+	, AVG(BaseRate) AS AvgRate
+	, AVG(VacationHours) AS AvgVacationHours
+	, MAX(SickLeaveHours) AS MaxSickLeaveHours
+  FROM AdventureWorks2012.Employee 
+  GROUP BY  DepartmentName, Gender, MaritalStatus;
+  --returns 51 rows
+
+  SELECT DepartmentName AS Department
+	, Gender
+	, MaritalStatus
+	, AVG(BaseRate) AS AvgRate
+	, AVG(VacationHours) AS AvgVacationHours
+	, MAX(SickLeaveHours) AS MaxSickLeaveHours
+  FROM AdventureWorks2012.Employee 
+  GROUP BY ROLLUP ( DepartmentName, Gender, MaritalStatus);
+  /* returns 98 rows
+   with a hierarchy formed by the input elements ( DepartmentName, Gender, MaritalStatus)
+   and NUlls at the result are used as placeholders when the element is not  part of the grouping set.
+   ROLLUP provides totals using the elements in ( DepartmentName, Gender, MaritalStatus)
+
+   There are aggregations:
+		for each department
+			for each gender group in this department
+				for each marital status in each group
+		grand total (at the end of the result set)
+ */
+
+    SELECT DepartmentName AS Department
+	, Gender
+	, MaritalStatus
+	, AVG(BaseRate) AS AvgRate
+	, AVG(VacationHours) AS AvgVacationHours
+	, MAX(SickLeaveHours) AS MaxSickLeaveHours
+  FROM AdventureWorks2012.DimEmployee 
+  GROUP BY  DepartmentName, Gender, MaritalStatus
+  WITH ROLLUP ;
+  -- returns with 98 rows, the result is the same as from the above query
+
+  USE SQLClass ; -- use SQLClass database
+  GO
+
+    SELECT Region
+	, CountryCode
+	, CardType
+	, SUM(TotalDue)
+  FROM SQLClass.Sales_CreditCard 
+  GROUP BY 
+	Region
+	, CountryCode
+	, CardType 
+  ORDER BY 
+	Region
+	, CountryCode
+	, CardType ; -- returns 24 rows
+
+
+
+	  SELECT Region
+	, CountryCode
+	, CardType
+	, SUM(TotalDue)
+  FROM SQLClass.Sales_CreditCard 
+  GROUP BY 
+	Region
+	, CountryCode
+	, CardType 
+  WITH ROLLUP
+  ORDER BY 
+	Region
+	, CountryCode
+	, CardType ; -- returns 34 rows
+
+	-- or
+ SELECT Region
+	, CountryCode
+	, CardType
+	, SUM(TotalDue)
+ From SQLClass.Sales_CreditCard
+ GROUP BY ROLLUP 
+  (
+	Region
+	, CountryCode
+	, CardType 
+	)
+  ORDER BY 
+	Region
+	, CountryCode
+	, CardType ; -- returns 34 rows 
